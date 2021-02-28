0980c33edd569003182c300870b17a588d6c2755
Las <haho013039@gmail.com>
2021-02-28 13:37:26 +0900

    repositroy에서 다음 식별자를 가져오는 테스트 작성

--- artist/src/test/kotlin/adapter/ArtistRepositoryTest.kt
+++ artist/src/test/kotlin/adapter/ArtistRepositoryTest.kt
@@ -5,6 +5,8 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 import strikt.api.expectThat
 import strikt.assertions.contains
+import strikt.assertions.hasSize
+import strikt.assertions.isEmpty
 
 class ArtistRepositoryTest: Spek({
     Feature("아티스트 저장하기") {
@@ -21,6 +23,28 @@ class ArtistRepositoryTest: Spek({
             }
             Then("아티스트를 찾을 수 있다."){
                 expectThat(repository.findAllArtists()).contains(artist)
+            }
+        }
+    }
+    Feature("다음 식별자 생성하기") {
+        val repository = InMemoryArtistRepository()
+        lateinit var nextId: String
+
+        Scenario("식별자 얻어오기") {
+
+            Given("아티스트가 저장이 되어있을 때"){
+                val artist = Artist("1", "Las", "Las is magical girl")
+                repository.save(artist)
+            }
+            When("다음 식별자를 얻는 경우"){
+                nextId = repository.nextId()
+            }
+            Then("얻은 식별자로 저장되어 있는 artist가 존재하지 않는다."){
+                expectThat(
+                    repository.findAllArtists().filter {
+                        it.id == nextId
+                    }
+                ).hasSize(0)
             }
         }
     }
